f6e11bdcaf18751d5e330fc0fdecb6956d8118c1
Thomas <tway@cloudflare.com>
2025-03-04 15:22:41 +0000

    post interview

--- api/api.go
+++ api/api.go
@@ -29,23 +29,68 @@ type npmPackageResponse struct {
 }
 
 type NpmPackageVersion struct {
-	Name         string                        `json:"name"`
-	Version      string                        `json:"version"`
+	parent *NpmPackageVersion
+
+	Name    string `json:"name"`
+	Version string `json:"version"`
+
+	// review: I'm concerned that this is changing the contract of the API.
+	// It's not clear to me as to what capacity this service is being used,
+	// but existing clients will be expecting a `map[string]string` and will
+	// fail to decode this.
+	//
+	// idea: Assuming that breaking changes are unacceptable, we could use a
+	// new route which lists dependencies recursively.
 	Dependencies map[string]*NpmPackageVersion `json:"dependencies"`
+}
+
+func (pkg *NpmPackageVersion) HasDirectAncestor(target *NpmPackageVersion) bool {
+	if pkg.parent == nil {
+		return false
+	}
+	if pkg.Is(target) {
+		return true
+	}
+	return pkg.parent.HasDirectAncestor(target)
+}
+
+func (pkg *NpmPackageVersion) Is(target *NpmPackageVersion) bool {
+	return pkg.Name == target.Name && pkg.Version == target.Version
 }
 
 func packageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	pkgName := vars["package"]
+
+	// review: It is important to note that this is actually a constraint
+	// and note a version. I'm not sure what sort of consequences this may
+	// have, or if it's a bad thing, but it is unexpected.
 	pkgVersion := vars["version"]
 
 	rootPkg := &NpmPackageVersion{Name: pkgName, Dependencies: map[string]*NpmPackageVersion{}}
-	if err := resolveDependencies(rootPkg, pkgVersion); err != nil {
+	if err := newDependencyResolver().resolveDependencies(rootPkg, pkgVersion); err != nil {
+		// review: I understand a precedent has already been set in this
+		// project, but we should be using a structured logging package
+		// and adding context to errors. Printing a plain error message
+		// to stdout without any context is not really helpful.
 		println(err.Error())
+
+		// review: We should be using constants like
+		// http.StatusInternalServerError instead of magic numbers.
+		//
+		// review: Is this the best response code to use? What if the
+		// package simply can't be found? That would be a 404, not a
+		// 500.
 		w.WriteHeader(500)
 		return
 	}
 
+	// idea: It may be nice to use [json.NewEncoder] instead. It should be
+	// more efficient and is more concise.
+	//
+	// idea: I recognise indentation is helpful for humans and debugging,
+	// but should a production system be serving indented json? It's a lot
+	// of exta data.
 	stringified, err := json.MarshalIndent(rootPkg, "", "  ")
 	if err != nil {
 		println(err.Error())
@@ -54,14 +99,75 @@ func packageHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+
+	// idea: A 200 OK response is implied if no status code is explicitly
+	// sent and as such this is kind of redundant.
 	w.WriteHeader(200)
 
+	// idea: Explicitly ignoring the return value here is only ever done to
+	// appease linters and generally is discouraged.
+	//
 	// Ignoring ResponseWriter errors
 	_, _ = w.Write(stringified)
 }
 
-func resolveDependencies(pkg *NpmPackageVersion, versionConstraint string) error {
-	pkgMeta, err := fetchPackageMeta(pkg.Name)
+type dependencyResolver struct {
+	metaCache    map[string]*npmPackageMetaResponse
+	packageCache map[string]*npmPackageResponse
+}
+
+func newDependencyResolver() *dependencyResolver {
+	return &dependencyResolver{
+		metaCache:    make(map[string]*npmPackageMetaResponse),
+		packageCache: make(map[string]*npmPackageResponse),
+	}
+}
+
+// review: I like that this functionality has been moved into a function! It's
+// much cleaner this way. I recognise it's necessary due to being recursive, but
+// even so.
+//
+// review: I tried this against express@4.21.2, and it took a very long time to
+// load. Is there anyway we can make it faster?
+//
+// review: Have we considered circular dependencies? Expressing them in a tree
+// like this is basically impossible, so I guess the best thing to do would be
+// to stop recursing if the child is also an ancestor (or itself).
+//
+// That said, even if we stop recursing when a package has a child of itself,
+// the tree could still be huge if the same package is used by multiple other
+// packages. I guess this is a long way of saying that a tree is probably not
+// the most efficient way of representing this data. It really calls for a
+// proper graph rather than a tree.
+//
+// idea: We could have a cache, both for package metadata and for packages
+// themselves. This may help with speed.
+//
+// review: I tried this against npm@11.1.0 and received the following error:
+//
+//	improper constraint: npm:strip-ansi@^6.0.1
+//
+// This seems strange, as it looks like the code tries to account for this.
+//
+// Source of the error:
+//
+//	https://github.com/Masterminds/semver/blob/1558ca3488226e3490894a145e831ad58a5ff958/constraints.go#L32
+//
+//	https://github.com/Masterminds/semver/blob/1558ca3488226e3490894a145e831ad58a5ff958/constraints.go#L245
+//
+// review: I also tested trucolor@4.0.4 and it never finished loading. I can
+// only assume it has a circular dependency.
+//
+// review: The task suggests fetching packages async to help improve speed. I
+// think this is a nice idea, though it is lower on the priority list than
+// caching and resolving circular dependencies. Fetching dependencies async may
+// make the code more complicated and considerations then need to be made for
+// data races. It's absolutely doable with tools like
+// [https://pkg.go.dev/golang.org/x/sync/errgroup],
+// [https://pkg.go.dev/golang.org/x/sync/singleflight] and [sync.Mutex].
+// Complexity is my concern.
+func (r *dependencyResolver) resolveDependencies(pkg *NpmPackageVersion, versionConstraint string) error {
+	pkgMeta, err := r.fetchPackageMeta(pkg.Name)
 	if err != nil {
 		return err
 	}
@@ -71,14 +177,22 @@ func resolveDependencies(pkg *NpmPackageVersion, versionConstraint string) error
 	}
 	pkg.Version = concreteVersion
 
-	npmPkg, err := fetchPackage(pkg.Name, pkg.Version)
+	if pkg.parent != nil && pkg.parent.HasDirectAncestor(pkg) {
+		return nil
+	}
+
+	npmPkg, err := r.fetchPackage(pkg.Name, pkg.Version)
 	if err != nil {
 		return err
 	}
 	for dependencyName, dependencyVersionConstraint := range npmPkg.Dependencies {
-		dep := &NpmPackageVersion{Name: dependencyName, Dependencies: map[string]*NpmPackageVersion{}}
+		dep := &NpmPackageVersion{
+			parent:       pkg,
+			Name:         dependencyName,
+			Dependencies: map[string]*NpmPackageVersion{},
+		}
 		pkg.Dependencies[dependencyName] = dep
-		if err := resolveDependencies(dep, dependencyVersionConstraint); err != nil {
+		if err := r.resolveDependencies(dep, dependencyVersionConstraint); err != nil {
 			return err
 		}
 	}
@@ -112,24 +226,61 @@ func filterCompatibleVersions(constraint *semver.Constraints, pkgMeta *npmPackag
 	return compatible
 }
 
-func fetchPackage(name, version string) (*npmPackageResponse, error) {
+func (r *dependencyResolver) fetchPackage(name, version string) (*npmPackageResponse, error) {
+	if v, ok := r.packageCache[name+version]; ok {
+		fmt.Printf("using cached package: %s@%s\n", name, version)
+		return v, nil
+	}
+
+	// idea: URL construction could be safer with [path.Join] and
+	// [net/url.URL.ResolveReference]. String templating is prone to all
+	// sorts of weirdness.
+	//
+	// Example:
+	//
+	// 	u.ResolveReference(&url.URL{
+	//		Path: path.Join(name, version),
+	// 	})
+	//
+	// idea: It has historically been strongly encouraged to construct
+	// bespoke a [*http.Client] with reasonable timeouts. It is not so
+	// relevant these days as the default transport has pretty reasonable
+	// defaults now. That said, a bespoke [*http.Client] may still be worth
+	// considering as it would be useful for collecting metrics and tracing.
 	resp, err := http.Get(fmt.Sprintf("https://registry.npmjs.org/%s/%s", name, version))
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
+	// idea: There should be some sort of limit. It's possible the response
+	// body could be large enough to cause memory exhaustion.
+	//
+	// idea: In addition, it's probably better to [json.NewDecoder] where
+	// possible.
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
+	// idea: We should not be ignoring decoding errors.
 	var parsed npmPackageResponse
 	_ = json.Unmarshal(body, &parsed)
+
+	r.packageCache[name+version] = &parsed
+
 	return &parsed, nil
 }
 
-func fetchPackageMeta(p string) (*npmPackageMetaResponse, error) {
+// idea: If the package is not found (404, I assume), we should return a
+// different error which the handler can use to differentiate between fatal and
+// non fatal.
+func (r *dependencyResolver) fetchPackageMeta(p string) (*npmPackageMetaResponse, error) {
+	if v, ok := r.metaCache[p]; ok {
+		fmt.Printf("using cached package meta: %s\n", p)
+		return v, nil
+	}
+
 	resp, err := http.Get(fmt.Sprintf("https://registry.npmjs.org/%s", p))
 	if err != nil {
 		return nil, err
@@ -145,6 +296,8 @@ func fetchPackageMeta(p string) (*npmPackageMetaResponse, error) {
 	if err := json.Unmarshal([]byte(body), &parsed); err != nil {
 		return nil, err
 	}
+
+	r.metaCache[p] = &parsed
 
 	return &parsed, nil
 }
--- api/api_test.go
+++ api/api_test.go
@@ -14,11 +14,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// review: There are very few tests for other happy paths, non happy paths and
+// pathological cases like circular dependencies, or both deep and wide trees.
+// What happens if the given version if not accepted by the semver package? What
+// if the response body from npmjs.com isn't json? What is the response is
+// abnormal (not 2xx)?
+//
+// review: I already wrote something similar to this elsewhere, but these tests
+// should be reproducible. I tried running the test, and it failed, because it's
+// using an external system. The test should use a mock http server instead.
 func TestPackageHandler(t *testing.T) {
 	handler := api.New()
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
+	// idea: instead of relying on npmjs.com for unit tests, it would be
+	// better to create a mock http server and use that instead. Unit tests
+	// should be offline and reproducible.
 	resp, err := server.Client().Get(server.URL + "/package/react/16.13.0")
 	require.Nil(t, err)
 	defer resp.Body.Close()
@@ -35,9 +47,56 @@ func TestPackageHandler(t *testing.T) {
 	assert.Equal(t, "16.13.0", data.Version)
 
 	fixture, err := os.Open(filepath.Join("testdata", "react-16.13.0.json"))
+
+	// review: Prefer require.NoError. The intention of the assertion is
+	// clearer, and testify should present it better than just the value not
+	// being nil.
+	//
+	// review: This is probably a distraction, sorry, it would be good to
+	// establish some guidelines for the project. Google's Go style guide
+	// discourages use of assertion helps like testify as it is not
+	// considered idiomatic. I really see the value though and have used
+	// testify extensively myself.
+	//
+	//      https://google.github.io/styleguide/go/best-practices#leave-testing-to-the-test-function
+	//
+	// 	https://google.github.io/styleguide/go/decisions#assert
 	require.Nil(t, err)
+
 	var fixtureObj api.NpmPackageVersion
 	require.Nil(t, json.NewDecoder(fixture).Decode(&fixtureObj))
 
+	// review: Is this the best way to check the API is returning what we
+	// expect from it? We are comparing json which has been decoded into the
+	// same struct. What is there are extra fields being encoded which
+	// shouldn't be there, or if the structure of the json changes. This
+	// won't necessarily protect against regressions or breaking changes in
+	// the API. Go's json encoder is deterministic, so it might be a better
+	// idea to both validate that the response decodes okay, and compare the
+	// raw response body with the testdata. [json.Compact] may be helpful
+	// for this, as it will ensure the two json strings are comparable
+	// without consideration for whitespace.
 	assert.Equal(t, fixtureObj, data)
+}
+
+func TestCycle(t *testing.T) {
+	handler := api.New()
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	resp, err := server.Client().Get(server.URL + "/package/d/1.0.1")
+	require.Nil(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	require.Nil(t, err)
+
+	var data api.NpmPackageVersion
+	err = json.Unmarshal(body, &data)
+	require.Nil(t, err)
+
+	assert.Equal(t, "d", data.Name)
+	assert.Equal(t, "1.0.1", data.Version)
 }
